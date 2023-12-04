dd85e2ccd144739892ac2f44281f45ab6a67c356
Codemartino <akudugumartin5@gmail.com>
2023-12-04 03:30:17 +0000

    fixed

--- backend/admin/assets/inc/nav.php
+++ backend/admin/assets/inc/nav.php
@@ -67,12 +67,12 @@
         <div class="logo-box">
             <a href="his_admin_dashboard.php" class="logo text-center">
                 <span class="logo-lg">
-                    <img src="assets/images/logo-light.png" alt="" height="18">
+                    <img src="assets/images/pharm.png" alt="" height="18">
                     <!-- <span class="logo-lg-text-light">UBold</span> -->
                 </span>
                 <span class="logo-sm">
                     <!-- <span class="logo-sm-text-dark">U</span> -->
-                    <img src="assets/images/logo-sm-white.png" alt="" height="24">
+                    <img src="assets/images/pharm.png" alt="" height="24">
                 </span>
             </a>
         </div>
